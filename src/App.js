6f624705d2b043deb458785a8919864e76117dde
SabrinaCostaa <sfcosta27@gmail.com>
2025-12-08 18:50:28 -0300

    Aulas 01-04 do curso: Introdução e Criação de Coponentes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './components/HelloWord';
 
 function App() {
+
+  const name = "Sabrina";
+  const newName = name.toUpperCase();
+  const url = 'https://via.placeholder.com/150'
+
+  function sum(a,b){
+    return a+b;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Alternado JSX</h2>
+      <p>Olá {name}!</p>
+      <p>Olá {newName}!</p>
+      <p>Soma : {sum(1,2)}</p>
+      <img src = {url} alt = "Minha Imagem" />
+      <HelloWorld/>
     </div>
   );
 }
